8588568da99a6213df10af6f40c2f15389b4cdb5
Gaurav Vinayak Gaikwad <61324204+Gaurav-223344@users.noreply.github.com>
2022-09-23 13:25:11 +0530

    Add files via upload

--- ArrayPractice/wordGame/index.js
+++ ArrayPractice/wordGame/index.js
@@ -13,12 +13,11 @@ function isAlreadyAdded(array, element) {
 
 
 function shuffleNumbers(maxRange, numbersWeWant) {
-    if (numbersWeWant<=maxRange){
+    if (numbersWeWant <= maxRange) {
         arrayForShuffledNumbers = [];
         index = 0
         while (arrayForShuffledNumbers.length < numbersWeWant) {
             randomNumber = randomNumberGenerator(maxRange)
-            // console.log(randomNumber)
             if (!isAlreadyAdded(arrayForShuffledNumbers, randomNumber)) {
                 arrayForShuffledNumbers.push(randomNumber)
                 index++
@@ -30,23 +29,73 @@ function shuffleNumbers(maxRange, numbersWeWant) {
 
 
 
-function shuffleWord(word){
+function shuffleWord(word,number) {
+    document.getElementById('number').innerText = number;
     arrayOfWord = word.split('')
-    indexOfShuffledWord = shuffleNumbers(word.length,word.length)
+    indexOfShuffledWord = shuffleNumbers(word.length, word.length)
     arrayOfShuffledWord = []
-    for (let i = 0; i<indexOfShuffledWord.length; i++){
+    
+    for (let i = 0; i < indexOfShuffledWord.length; i++) {
         arrayOfShuffledWord[i] = arrayOfWord[indexOfShuffledWord[i]]
     }
-    console.log(arrayOfShuffledWord)
 
-    for (let i=0; i<indexOfShuffledWord.length; i++){
-        idOfDivTag = document.getElementById('box_'+i)
+    for (let i = 0; i < indexOfShuffledWord.length; i++) {
+        idOfDivTag = document.getElementById('box_' + i)
+        document.getElementById('box_' + i).classList.add('letterContainer')
+        document.getElementById('letter_' + i).classList.remove('hidden')
         idOfDivTag.innerHTML = arrayOfShuffledWord[i];
-        idOfDivTag.addEventListener('click',function(event){
+        idOfDivTag.value = arrayOfShuffledWord[i]
+    }
+    
+}
+
+function clientResponse(word){
+    arrayOfClientResponse = '';
+    letterBoxId = 0;
+    for (let i = 0; i < word.length; i++) {
+        document.getElementById('box_' + i).addEventListener('click', function (event) {
+            document.getElementById('box_' + i).classList.add('clientResponse')
             
-            idOfDivTag.classList.add('')
+            clientResponseLetter = document.getElementById('box_' + i).value
+            if (document.getElementById('box_' + i).attributes.isClicked.value === 'false'){
+                arrayOfClientResponse += clientResponseLetter
+                document.getElementById('letter_' + letterBoxId).value = clientResponseLetter
+                document.getElementById('box_' + i).attributes.isClicked.value = 'true'
+                letterBoxId++
+            }
+            if(arrayOfClientResponse.length === word.length){
+                console.log(arrayOfClientResponse)
+                if(arrayOfClientResponse === word){
+                    document.getElementById('statement').innerText = 'Winner Winner Chicken Dinner'
+                }else{
+                    document.getElementById('statement').innerText = 'Sorry, Better luck next time'
+                }
+            }             
         })
+        
+
     }
 }
 
-shuffleWord('FOUR')
+
+
+
+function wordGame(word,number){
+    
+    shuffleWord(word,number)
+    clientResponse(word)
+}
+
+
+function randomSpellings(){
+    spellingOfNumbers = ['ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE','TEN']
+    // spellingOfNumbers = ['FOUR','FIVE','NINE']
+    indexOfSpellings = randomNumberGenerator(spellingOfNumbers.length)
+    // console.log(indexOfSpellings+1)
+    // console.log(spellingOfNumbers[indexOfSpellings])
+    wordGame(spellingOfNumbers[indexOfSpellings],indexOfSpellings+1);
+}
+
+// randomSpellings()
+
+wordGame('THREE',3)
